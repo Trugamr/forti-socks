4687affa0d0b57b07cef598299d655c8a4e302c7
Trugamr <trugamr@live.in>
2024-09-10 17:03:54 +0530

    feat: add healthcheck and depend on vpn

--- socks/start.sh
+++ socks/start.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# TODO: Add health check to vpn container and add dependency on it or use sock5 server
-echo "Waiting for OpenFortiVPN to start..."
-sleep 10
-
 echo "Starting SOCKS proxy..."
+
 sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no -D 0.0.0.0:1080 -q -C -N "$SSH_USER@$SSH_HOST"
