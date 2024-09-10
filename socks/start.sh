8146abb1f23a7879ccf29121ec6a4873de7bcf34
Trugamr <trugamr@live.in>
2024-09-10 19:06:59 +0530

    feat: use slim images

--- socks/start.sh
+++ socks/start.sh
@@ -4,4 +4,18 @@ set -e
 
 echo "Starting SOCKS proxy..."
 
-sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no -D 0.0.0.0:1080 -q -C -N "$SSH_USER@$SSH_HOST"
+while true; do
+    echo "Attempting to connect SOCKS proxy..."
+    
+    # Add ConnectTimeout, ServerAliveInterval, ServerAliveCountMax, and disable TCPKeepAlive
+    sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no \
+        -o ConnectTimeout=10 \
+        -o ServerAliveInterval=10 \
+        -o ServerAliveCountMax=3 \
+        -o TCPKeepAlive=no \  # Disable TCP-level keepalive
+        -D 0.0.0.0:1080 -q -C -N "$SSH_USER@$SSH_HOST"
+    
+    # If ssh fails, wait and retry
+    echo "Connection lost, retrying in 5 seconds..."
+    sleep 5
+done
